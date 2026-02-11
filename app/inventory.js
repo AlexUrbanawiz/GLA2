21338bb827e9c91243debd1242bb8855a47a7608
nblGuildenstern <122514768+nblGuildenstern@users.noreply.github.com>
2026-02-11 10:59:17 -0700

    Added Contexts

--- app/inventory.js
+++ app/inventory.js
@@ -1,6 +1,10 @@
 import { Text, View } from 'react-native';
+import { useInventory } from './context/InventoryContext';
 
 export default function Inventory() {
+  const {addItem, isAdded} = useInventory();
+  
+
   return (
     <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
       <Text>Inventory</Text>
