52289df28cf890d1f13dd2d23b0b8cc61682af1c
Brita McNabb <brita@mcnabbs.org>
2026-02-17 10:49:54 -0700

    testing checkboxes

--- app/grocery_list.js
+++ app/grocery_list.js
@@ -1,9 +1,37 @@
-import { Text, View } from 'react-native';
+import Checkbox from 'expo-checkbox';
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 
-export default function Grocery() {
+export default function Inventory() {
+  const [isChecked, setChecked] = useState(false);
+
   return (
-    <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
-      <Text>Grocery</Text>
+    <View style={styles.container}>
+      <View style={styles.section}>
+        <Checkbox style={styles.checkbox} value={isChecked} onValueChange={setChecked} />
+        <Text style={styles.paragraph}>
+          {/* {ingredient("test", 1, 5)} */}
+          test
+        </Text>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 16,
+    marginVertical: 32,
+  },
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  paragraph: {
+    fontSize: 15,
+  },
+  checkbox: {
+    margin: 8,
+  },
+});
