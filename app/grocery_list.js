000ebb6b9bef93ee830bbf0934754265c940cb59
Alex Urbanawiz <64279311+AlexUrbanawiz@users.noreply.github.com>
2026-02-18 10:52:54 -0700

    Main into Sam (#63)
    * Project now runs
    
    * testing checkboxes
    
    * Improvements to recipes
    
    * Added InventoryContext to save
    
    * more checklist stuff, not working
    
    ---------
    
    Co-authored-by: Brita McNabb <brita@mcnabbs.org>
    Co-authored-by: nblGuildenstern <122514768+nblGuildenstern@users.noreply.github.com>

--- app/grocery_list.js
+++ app/grocery_list.js
@@ -1,9 +1,48 @@
-import { Text, View } from 'react-native';
+import Checkbox from 'expo-checkbox';
+// import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+// import { Button } from 'react-native-web';
 
-export default function Grocery() {
+export default function Inventory() {
+  // const [isChecked, setChecked] = useState([
+  // ]);
+
   return (
-    <View style={{ flex:1, alignItems:'center', justifyContent:'center' }}>
-      <Text>Grocery</Text>
+    <View style={styles.container}>
+      {/* <View style={styles.section}>
+        <Button>
+          onPress={addItem()};
+        </Button>
+      </View> */}
+      <View style={styles.section}>
+        <Checkbox style={styles.checkbox} value={isChecked} onValueChange={setChecked} />
+        <Text style={styles.paragraph}>
+          test
+        </Text>
+      </View>
     </View>
   );
 }
+
+// const addItem = (item, quantity, price) => {
+//   item = new Ingredient(item, quantity, price);
+//   setChecked(item);
+// }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginHorizontal: 16,
+    marginVertical: 32,
+  },
+  section: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  paragraph: {
+    fontSize: 15,
+  },
+  checkbox: {
+    margin: 8,
+  },
+});
---
+++ classes/ingredient.js
@@ -0,0 +1,18 @@
+// name, quantity, price
+
+export default class Ingredient {
+  constructor(item, quantity, price) {
+    this.item = item;
+    this.quantity = quantity;
+    this.price = price;
+  }
+  get_name() {
+    return this.item;
+  }
+  get_quantity() {
+    return this.quantity;
+  }
+  get_price() {
+    return this.price;
+  }
+}
