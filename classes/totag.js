6905f597ddc02d1f901b6267cd33adf35d75bb6f
Alex Urbanawiz <64279311+AlexUrbanawiz@users.noreply.github.com>
2026-02-18 11:02:40 -0700

    Main into Nathan (#64)
    * Project now runs
    
    * testing checkboxes
    
    * Improvements to recipes
    
    * Added InventoryContext to save
    
    * more checklist stuff, not working
    
    ---------
    
    Co-authored-by: Brita McNabb <brita@mcnabbs.org>
    Co-authored-by: nblGuildenstern <122514768+nblGuildenstern@users.noreply.github.com>

---
+++ classes/totag.js
@@ -0,0 +1,31 @@
+// class ToTag {
+//   constructor(name, tags = []) {
+//     this.id = null;
+//     this.id = name;
+//     this.id = tags;
+//   }
+
+//   addTag(tag) {
+//     // Check if tag already exists in this.tags
+//     if (this.tags.includes(tag)) {
+//       return false; // Tag already exists
+//     }
+//     // If tag does not already exist add to list
+//     this.tags.push(tag);
+//     return true; // Adds ToTag
+//   }
+
+//   removeTag(tag) {
+//     // Remove a tag from the ingredient
+//   }
+
+//   hasTag(tag) {
+//     // Check if ingredient has a specific tag
+//   }
+
+//   getTags() {
+//     // Return all tags for this ingredient
+//   }
+// }
+
+// export default Ingredient;
