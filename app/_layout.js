f6a1091d5e6a13b2cc71ef63872104b052ebe646
Alex Urbanawiz <64279311+AlexUrbanawiz@users.noreply.github.com>
2026-02-18 11:03:46 -0700

    Main into Delano (#66)
    * Project now runs
    
    * testing checkboxes
    
    * Improvements to recipes
    
    * Added InventoryContext to save
    
    * more checklist stuff, not working
    
    * Quick Change (#65)
    
    ---------
    
    Co-authored-by: Brita McNabb <brita@mcnabbs.org>
    Co-authored-by: nblGuildenstern <122514768+nblGuildenstern@users.noreply.github.com>

--- app/_layout.js
+++ app/_layout.js
@@ -1,26 +1,29 @@
 // app/_layout.js
 import { Stack } from 'expo-router';
+import { InventoryProvider } from '../context/InventoryContext';
+
 
 export default function Layout() {
   return (
-    <Stack>
-      <Stack.Screen
-        name="index"
-        options={{
-          title: 'GLA',
-          headerRight: () => <HeaderButton />,
-        }}
-      />
-      <Stack.Screen name="settings" options={{ title: 'Settings' }} />
-      <Stack.Screen name="inventory" options={{ title: 'Inventory' }} />
-      <Stack.Screen name="grocery_list" options={{ title: 'Grocery List' }} />
-      <Stack.Screen name="budget" options={{ title: 'Budget' }} />
-      <Stack.Screen name="recipes" options={{ title: 'Recipes' }} />
-    </Stack>
+    <InventoryProvider>
+      <Stack>
+        <Stack.Screen
+          name="index"
+          options={{
+            title: 'GLA',
+            headerRight: () => <HeaderButton />,
+          }}
+          />
+        <Stack.Screen name="settings" options={{ title: 'Settings' }} />
+        <Stack.Screen name="inventory" options={{ title: 'Inventory' }} />
+        <Stack.Screen name="grocery_list" options={{ title: 'Grocery List' }} />
+        <Stack.Screen name="budget" options={{ title: 'Budget' }} />
+        <Stack.Screen name="recipes" options={{ title: 'Recipes' }} />
+      </Stack>
+    </InventoryProvider>
   );
 }
 
-// Your custom header button
 import { Ionicons } from '@expo/vector-icons';
 import { router } from 'expo-router';
 import { Pressable } from 'react-native';
